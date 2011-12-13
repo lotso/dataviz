84786c0767884380a2effcba2ca92b646ad94af6
Chamal Samaranayake <chamals@stanford.edu>
2011-12-12 20:48:10 -0800

    overhaul of open secrets.js and also added senators.js

---
+++ senators.js
@@ -0,0 +1,408 @@
+var senatorsObj = {
+	"Senators": [
+		{
+			"Name": "Akaka, Daniel K",
+			"ID": "N00007653"
+		},
+		{
+			"Name": "Alexander, Lamar",
+			"ID": "N00009888"
+		},
+		{
+			"Name": "Ayotte, Kelly A",
+			"ID": "N00030980"
+		},
+		{
+			"Name": "Barrasso, John A",
+			"ID": "N00006236"
+		},
+		{
+			"Name": "Baucus, Max",
+			"ID": "N00004643"
+		},
+		{
+			"Name": "Begich, Mark",
+			"ID": "N00029901"
+		},
+		{
+			"Name": "Bennet, Michael F",
+			"ID": "N00030608"
+		},
+		{
+			"Name": "Bingaman, Jeff",
+			"ID": "N00006518"
+		},
+		{
+			"Name": "Blumenthal, Richard",
+			"ID": "N00031685"
+		},
+		{
+			"Name": "Blunt, Roy",
+			"ID": "N00005195"
+		},
+		{
+			"Name": "Boozman, John",
+			"ID": "N00013873"
+		},
+		{
+			"Name": "Bordallo, Madeleine Z",
+			"ID": "N00024866"
+		},
+		{
+			"Name": "Boxer, Barbara",
+			"ID": "N00006692"
+		},
+		{
+			"Name": "Brown, Scott P",
+			"ID": "N00031174"
+		},
+		{
+			"Name": "Brown, Sherrod",
+			"ID": "N00003535"
+		},
+		{
+			"Name": "Burr, Richard",
+			"ID": "N00002221"
+		},
+		{
+			"Name": "Cantwell, Maria",
+			"ID": "N00007836"
+		},
+		{
+			"Name": "Cardin, Ben",
+			"ID": "N00001955"
+		},
+		{
+			"Name": "Carper, Tom",
+			"ID": "N00012508"
+		},
+		{
+			"Name": "Casey, Bob",
+			"ID": "N00027503"
+		},
+		{
+			"Name": "Chambliss, Saxby",
+			"ID": "N00002685"
+		},
+		{
+			"Name": "Coats, Daniel R",
+			"ID": "N00003845"
+		},
+		{
+			"Name": "Coburn, Tom",
+			"ID": "N00005601"
+		},
+		{
+			"Name": "Cochran, Thad",
+			"ID": "N00003328"
+		},
+		{
+			"Name": "Collins, Susan M",
+			"ID": "N00000491"
+		},
+		{
+			"Name": "Conrad, Kent",
+			"ID": "N00004613"
+		},
+		{
+			"Name": "Coons, Chris",
+			"ID": "N00031820"
+		},
+		{
+			"Name": "Corker, Bob",
+			"ID": "N00027441"
+		},
+		{
+			"Name": "Cornyn, John",
+			"ID": "N00024852"
+		},
+		{
+			"Name": "Crapo, Mike",
+			"ID": "N00006267"
+		},
+		{
+			"Name": "DeMint, James W",
+			"ID": "N00002472"
+		},
+		{
+			"Name": "Durbin, Dick",
+			"ID": "N00004981"
+		},
+		{
+			"Name": "Enzi, Mike",
+			"ID": "N00006249"
+		},
+		{
+			"Name": "Feinstein, Dianne",
+			"ID": "N00007364"
+		},
+		{
+			"Name": "Franken, Al",
+			"ID": "N00029016"
+		},
+		{
+			"Name": "Gillibrand, Kirsten",
+			"ID": "N00027658"
+		},
+		{
+			"Name": "Graham, Lindsey",
+			"ID": "N00009975"
+		},
+		{
+			"Name": "Grassley, Chuck",
+			"ID": "N00001758"
+		},
+		{
+			"Name": "Hagan, Kay R",
+			"ID": "N00029617"
+		},
+		{
+			"Name": "Harkin, Tom",
+			"ID": "N00004207"
+		},
+		{
+			"Name": "Hatch, Orrin G",
+			"ID": "N00009869"
+		},
+		{
+			"Name": "Heller, Dean",
+			"ID": "N00027522"
+		},
+		{
+			"Name": "Hutchison, Kay Bailey",
+			"ID": "N00005675"
+		},
+		{
+			"Name": "Inhofe, James M",
+			"ID": "N00005582"
+		},
+		{
+			"Name": "Inouye, Daniel K",
+			"ID": "N00001762"
+		},
+		{
+			"Name": "Isakson, Johnny",
+			"ID": "N00002593"
+		},
+		{
+			"Name": "Johanns, Michael O",
+			"ID": "N00029444"
+		},
+		{
+			"Name": "Johnson, Ron",
+			"ID": "N00032546"
+		},
+		{
+			"Name": "Johnson, Tim",
+			"ID": "N00010084"
+		},
+		{
+			"Name": "Kerry, John",
+			"ID": "N00000245"
+		},
+		{
+			"Name": "Kirk, Mark",
+			"ID": "N00012539"
+		},
+		{
+			"Name": "Klobuchar, Amy",
+			"ID": "N00027500"
+		},
+		{
+			"Name": "Kohl, Herb",
+			"ID": "N00004309"
+		},
+		{
+			"Name": "Kyl, Jon",
+			"ID": "N00006406"
+		},
+		{
+			"Name": "Landrieu, Mary L",
+			"ID": "N00005395"
+		},
+		{
+			"Name": "Lautenberg, Frank R",
+			"ID": "N00000659"
+		},
+		{
+			"Name": "Leahy, Patrick",
+			"ID": "N00009918"
+		},
+		{
+			"Name": "Lee, Mike",
+			"ID": "N00031696"
+		},
+		{
+			"Name": "Levin, Carl",
+			"ID": "N00001691"
+		},
+		{
+			"Name": "Lieberman, Joe",
+			"ID": "N00000616"
+		},
+		{
+			"Name": "Lugar, Richard G",
+			"ID": "N00001764"
+		},
+		{
+			"Name": "Manchin, Joe",
+			"ID": "N00032838"
+		},
+		{
+			"Name": "McCain, John",
+			"ID": "N00006424"
+		},
+		{
+			"Name": "McCaskill, Claire",
+			"ID": "N00027694"
+		},
+		{
+			"Name": "McConnell, Mitch",
+			"ID": "N00003389"
+		},
+		{
+			"Name": "Menendez, Robert",
+			"ID": "N00000699"
+		},
+		{
+			"Name": "Merkley, Jeff",
+			"ID": "N00029303"
+		},
+		{
+			"Name": "Mikulski, Barbara A",
+			"ID": "N00001945"
+		},
+		{
+			"Name": "Moran, Jerry",
+			"ID": "N00005282"
+		},
+		{
+			"Name": "Murkowski, Lisa",
+			"ID": "N00026050"
+		},
+		{
+			"Name": "Murray, Patty",
+			"ID": "N00007876"
+		},
+		{
+			"Name": "Nelson, Ben",
+			"ID": "N00005329"
+		},
+		{
+			"Name": "Nelson, Bill",
+			"ID": "N00009926"
+		},
+		{
+			"Name": "Norton, Eleanor Holmes",
+			"ID": "N00001692"
+		},
+		{
+			"Name": "Paul, Rand",
+			"ID": "N00030836"
+		},
+		{
+			"Name": "Portman, Rob",
+			"ID": "N00003682"
+		},
+		{
+			"Name": "Pryor, Mark",
+			"ID": "N00013823"
+		},
+		{
+			"Name": "Reed, Jack",
+			"ID": "N00000362"
+		},
+		{
+			"Name": "Reid, Harry",
+			"ID": "N00009922"
+		},
+		{
+			"Name": "Risch, James E",
+			"ID": "N00029441"
+		},
+		{
+			"Name": "Roberts, Pat",
+			"ID": "N00005285"
+		},
+		{
+			"Name": "Rockefeller, Jay",
+			"ID": "N00001685"
+		},
+		{
+			"Name": "Rubio, Marco",
+			"ID": "N00030612"
+		},
+		{
+			"Name": "Sanders, Bernie",
+			"ID": "N00000528"
+		},
+		{
+			"Name": "Schumer, Charles E",
+			"ID": "N00001093"
+		},
+		{
+			"Name": "Sessions, Jeff",
+			"ID": "N00003062"
+		},
+		{
+			"Name": "Shaheen, Jeanne",
+			"ID": "N00024790"
+		},
+		{
+			"Name": "Shelby, Richard C",
+			"ID": "N00009920"
+		},
+		{
+			"Name": "Snowe, Olympia J",
+			"ID": "N00000480"
+		},
+		{
+			"Name": "Stabenow, Debbie",
+			"ID": "N00004118"
+		},
+		{
+			"Name": "Tester, Jon",
+			"ID": "N00027605"
+		},
+		{
+			"Name": "Thune, John",
+			"ID": "N00004572"
+		},
+		{
+			"Name": "Toomey, Pat",
+			"ID": "N00001489"
+		},
+		{
+			"Name": "Udall, Mark",
+			"ID": "N00008051"
+		},
+		{
+			"Name": "Udall, Tom",
+			"ID": "N00006561"
+		},
+		{
+			"Name": "Vitter, David",
+			"ID": "N00009659"
+		},
+		{
+			"Name": "Warner, Mark",
+			"ID": "N00002097"
+		},
+		{
+			"Name": "Webb, James",
+			"ID": "N00028058"
+		},
+		{
+			"Name": "Whitehouse, Sheldon",
+			"ID": "N00027533"
+		},
+		{
+			"Name": "Wicker, Roger",
+			"ID": "N00003280"
+		},
+		{
+			"Name": "Wyden, Ron",
+			"ID": "N00007724"
+		}
+	]
+}
